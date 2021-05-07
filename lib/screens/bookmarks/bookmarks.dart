9f8554c0453f6e3c216f21f3a3d593c0b53f95a9
faseeh1999(LinuxWorldxZorin) <faseehshahzad.fs@gmail.com>
2021-05-07 20:29:08 +0500

    Bookmarks UI Started
    Bookmarks UI Started

--- lib/screens/bookmarks/bookmarks.dart
+++ lib/screens/bookmarks/bookmarks.dart
@@ -1,9 +1,11 @@
 import 'package:find_my_school_updated/shared/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class BookmarksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
@@ -16,8 +18,67 @@ class BookmarksScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text("Bookmarks Screen"),
+      body: Container(
+        width: size.width,
+        height: size.height * 0.3,
+        child: Column(
+          children: [
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: size.height * 0.05,
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: size.height * 0.05,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(
+                            fontFamily: 'ss',
+                            fontSize: size.width * 0.05,
+                            fontWeight: FontWeight.normal),
+                      ),
+                      SizedBox(height: size.height * 0.02),
+                      RatingBar.builder(
+                        itemSize: size.width * 0.04,
+                        initialRating: 4.0, //widget.school.rating,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemBuilder: (context, _) => Icon(
+                          Icons.star,
+                          color: Colors.blueAccent,
+                        ),
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
+                      ),
+                    ],
+                  ),
+                  //color: Colors.blueAccent,
+                ),
+                Card(
+                  child: Column(
+                    children: [Text("Hola", style: TextStyle(fontSize: 22))],
+                  ),
+                  color: Colors.blueAccent,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
