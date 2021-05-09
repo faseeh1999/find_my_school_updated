1554f90b0a374c6267b0afb304407d78644b8538
faseeh1999(LinuxWorldxZorin) <faseehshahzad.fs@gmail.com>
2021-05-10 00:33:17 +0500

    Used Stream Proovider
    Used Stream Proovider

--- lib/screens/bookmarks/testscree.dart
+++ lib/screens/bookmarks/testscree.dart
@@ -7,6 +7,7 @@ import 'package:find_my_school_updated/screens/notification/noti_list.dart';
 import 'package:find_my_school_updated/screens/notification/noti_tile.dart';
 import 'package:find_my_school_updated/services/database.dart';
 import 'package:find_my_school_updated/shared/drawer.dart';
+import 'package:find_my_school_updated/shared/loader.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,38 +17,61 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
+  bool isLoading = false;
   final Notifications n1 = Notifications(title: "Heelo", message: "Testing");
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    Size size = MediaQuery.of(context).size;
-    return StreamBuilder<List<Notifications>>(
-        stream: DatabaseService(uid: user.uid).alerts,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List notifications = snapshot.data;
-            return Scaffold(
-              appBar: AppBar(
-                iconTheme: IconThemeData(color: Colors.black),
-                elevation: 0.0,
-                backgroundColor: Colors.white,
-                title: Text(
-                  "Notifications",
-                  style: TextStyle(color: Colors.black, fontFamily: "ss"),
-                ),
-                centerTitle: true,
-              ),
-              drawer: MainDrawer(),
-              body: ListView.builder(
-                  itemCount: notifications.length,
-                  itemBuilder: (context, index) {
-                    return TestTile(
-                      notification: notifications[index],
-                    );
-                  }),
-              // body: TestList(),
-            );
-          }
-        });
+    return StreamProvider<List<Notifications>>.value(
+      value: DatabaseService(uid: user.uid).alerts,
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          title: Text(
+            "Notifications",
+            style: TextStyle(color: Colors.black, fontFamily: "ss"),
+          ),
+          centerTitle: true,
+        ),
+        drawer: MainDrawer(),
+        body: TestList(),
+      ),
+    );
   }
 }
+
+// StreamBuilder<List<Notifications>>(
+//         stream: DatabaseService(uid: user.uid).alerts,
+//         builder: (context, snapshot) {
+//           if (snapshot.hasData) {
+//             List notifications = snapshot.data ?? [];
+
+//             return isLoading
+//                 ? Loading()
+//                 : Scaffold(
+//                     appBar: AppBar(
+//                       iconTheme: IconThemeData(color: Colors.black),
+//                       elevation: 0.0,
+//                       backgroundColor: Colors.white,
+//                       title: Text(
+//                         "Notifications",
+//                         style: TextStyle(color: Colors.black, fontFamily: "ss"),
+//                       ),
+//                       centerTitle: true,
+//                     ),
+//                     drawer: MainDrawer(),
+//                     body: ListView.builder(
+//                         itemCount: notifications.length,
+//                         itemBuilder: (context, index) {
+//                           return TestTile(
+//                             notification: notifications[index],
+//                           );
+//                         }),
+//                     // body: TestList(),
+//                   );
+//           } else {
+//             return Loading();
+//           }
+//         })
