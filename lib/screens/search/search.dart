c2a57510f378076f828c5ec1d72dcd8ea5e610b7
faseeh1999(PC) <faseehsahhzad.fs@gmail.com>
2021-06-02 13:46:44 +0500

    Refreshed Project
    Refreshed Project.

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -1,7 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:find_my_school_updated/services/search.dart';
 import 'package:find_my_school_updated/shared/drawer.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  var queryResultSet = [];
+  var tempSearchStore = [];
+
+  initiateSearch(value) {
+    if (value.length == 0) {
+      setState(() {
+        queryResultSet = [];
+        tempSearchStore = [];
+      });
+    }
+    var capitalizedValue =
+        value.subString(0, 1).toUpperCase() + value.subString(1);
+
+    if (queryResultSet.length == 0 && value.length == 1) {
+      print(value);
+      SearchService().searchByName(value).then((QuerySnapshot docs) {
+        for (int i = 0; i < docs.documents.length; ++i) {
+          queryResultSet.add(docs.documents[i].data);
+          setState(() {
+            tempSearchStore.add(queryResultSet[i]);
+          });
+        }
+      });
+    } else {
+      tempSearchStore = [];
+      queryResultSet.forEach((element) {
+        if (element['name'].toLowerCase().contains(value.toLowerCase()) ==
+            true) {
+          if (element['name'].toLowerCase().indexOf(value.toLowerCase()) == 0) {
+            setState(() {
+              tempSearchStore.add(element);
+            });
+          }
+        }
+      });
+    }
+    if (tempSearchStore.length == 0 && value.length > 1) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +64,52 @@ class SearchScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text("Search Screen"),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              onChanged: (val) {
+                initiateSearch(val);
+              },
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  contentPadding: const EdgeInsets.only(left: 25.0),
+                  hintText: "Search By Name",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0))),
+            ),
+          ),
+          SizedBox(height: 10),
+          GridView.count(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            crossAxisCount: 2,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+            primary: false,
+            shrinkWrap: true,
+            children: tempSearchStore.map((e) {
+              return builResultCard(e);
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget builResultCard(e) {
+    return Card(
+      color: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      elevation: 2.0,
+      child: Container(
+        child: Center(
+          child: Text(
+            e['name'],
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 20.0),
+          ),
+        ),
       ),
     );
   }
