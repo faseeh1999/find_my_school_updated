3ff7fbffa4737c85244b71563f97e27dee47059f
faseeh1999 <faseehshahzad.fs@gmail.com>
2021-07-11 16:42:04 +0500

    Province Search Completed.
    Province Search Completed.

---
+++ lib/screens/search/province.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:find_my_school_updated/screens/search/resultProvince.dart';
+import 'package:find_my_school_updated/theme/color.dart';
+import 'package:page_transition/page_transition.dart';
+
+class Province extends StatefulWidget {
+  @override
+  _ProvinceState createState() => _ProvinceState();
+}
+
+class _ProvinceState extends State<Province> {
+  List provinceList = ['Punjab', 'Sindh', 'KPK', 'Balochistan', 'Azad Kashmir'];
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "Province Search",
+          style: TextStyle(color: Colors.black, fontFamily: "ss"),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: size.height * 0.3,
+              width: size.width,
+              color: primaryColor,
+              child: Center(
+                child: Text(
+                  "Search using Provinces",
+                  style: TextStyle(
+                    fontFamily: 'ss',
+                    color: Colors.white,
+                    fontSize: size.width * 0.07,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SizedBox(
+              height: size.height * 0.4,
+              child: GridView.count(
+                padding: EdgeInsets.all(size.width * 0.05),
+                shrinkWrap: false,
+
+                // crossAxisSpacing: size.width * .01,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 0,
+                //mainAxisSpacing: size.height * 0.01,
+                // Create a grid with 2 columns. If you change the scrollDirection to
+                // horizontal, this produces 2 rows.
+                crossAxisCount: 2,
+                // Generate 100 widgets that display their index in the List.
+                children: List.generate(4, (index) {
+                  return Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: primaryColorDark,
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10)),
+                      //color: primaryColorDark,
+                      height: size.height * 0.1,
+                      width: size.width * 0.5,
+                      child: FlatButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                PageTransition(
+                                    child: SearchResultsProvince(
+                                      province: provinceList[index].toString(),
+                                    ),
+                                    type: PageTransitionType.fade));
+                          },
+                          child: Text(
+                            provinceList[index],
+                            style: TextStyle(
+                                fontFamily: 'ss',
+                                fontSize: size.width * 0.06,
+                                color: Colors.white),
+                          )),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.04,
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: primaryColorDark,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10)),
+                //color: primaryColorDark,
+                height: size.height * 0.1,
+                width: size.width * 0.7,
+                child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          PageTransition(
+                              child: SearchResultsProvince(
+                                province: provinceList[provinceList.length - 1]
+                                    .toString(),
+                              ),
+                              type: PageTransitionType.fade));
+                    },
+                    child: Text(
+                      provinceList[provinceList.length - 1],
+                      style: TextStyle(
+                          fontFamily: 'ss',
+                          fontSize: size.width * 0.06,
+                          color: Colors.white),
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/search/resultProvince.dart
+++ lib/screens/search/resultProvince.dart
@@ -1,65 +1,19 @@
-import 'dart:ui';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_my_school_updated/models/school.dart';
 import 'package:find_my_school_updated/screens/home/school_detail.dart';
-import 'package:find_my_school_updated/screens/search/category.dart';
-import 'package:find_my_school_updated/screens/search/province.dart';
-import 'package:find_my_school_updated/screens/search/sector.dart';
-import 'package:find_my_school_updated/shared/drawer.dart';
-import 'package:find_my_school_updated/theme/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:page_transition/page_transition.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchResultsProvince extends StatefulWidget {
+  final String province;
+  SearchResultsProvince({this.province});
   @override
-  _SearchScreenState createState() => _SearchScreenState();
+  _SearchResultsProvinceState createState() => _SearchResultsProvinceState();
 }
 
-class _SearchScreenState extends State<SearchScreen> {
-  List cityList = [
-    'Lahore',
-    'Karachi',
-    'Islamabad',
-    'Peshawar',
-    'Quetta',
-    'Multan',
-    'Faisalabad',
-    'Sialkot',
-    'Gujranwala',
-    'Larkana',
-    'Hyderabad',
-    'Rawalpindi',
-    'Abbotabad',
-    'Mingora',
-    'Mardan'
-  ];
-
-  List lowerFee = [1000, 1500, 2000, 2500, 2700, 3000];
-  List upperFee = [
-    3500,
-    4000,
-    5000,
-    6000,
-    7000,
-    8000,
-    9000,
-    10000,
-    12000,
-    15000,
-    20000
-  ];
-
+class _SearchResultsProvinceState extends State<SearchResultsProvince> {
   String searchString;
-  String searchCategory,
-      searchProvince,
-      searchSector,
-      searchCity,
-      searchLower,
-      searchUpper;
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -69,12 +23,11 @@ class _SearchScreenState extends State<SearchScreen> {
         elevation: 0.0,
         backgroundColor: Colors.white,
         title: Text(
-          "Search",
+          "Search Results",
           style: TextStyle(color: Colors.black, fontFamily: "ss"),
         ),
         centerTitle: true,
       ),
-      drawer: MainDrawer(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -90,142 +43,27 @@ class _SearchScreenState extends State<SearchScreen> {
               decoration: InputDecoration(
                   prefixIcon: Icon(Icons.search),
                   contentPadding: const EdgeInsets.only(left: 25.0),
-                  hintText: "Search By Name",
+                  hintText: "Filter Results",
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(4.0))),
             ),
           ),
           SizedBox(height: size.height * 0.02),
           Text(
-            "Search using these filters",
+            "Schools According to ${widget.province}",
             style: TextStyle(
-                fontFamily: 'ss',
                 fontSize: size.width * 0.05,
-                fontWeight: FontWeight.normal,
+                fontWeight: FontWeight.w400,
                 color: Colors.black,
-                letterSpacing: 0.5),
-          ),
-          SizedBox(height: size.height * 0.002),
-          Padding(
-            padding: EdgeInsets.all(size.width * 0.02),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: Colors.transparent,
-                            border: Border.all()),
-                        child: FlatButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                PageTransition(
-                                    child: Category(),
-                                    type: PageTransitionType.fade));
-                          },
-                          //style: TextStyle(),
-                          child: Text(
-                            "Category",
-                            style: TextStyle(
-                              fontFamily: 'ss',
-                              color: Colors.black,
-                              fontSize: size.width * 0.045,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: Colors.transparent,
-                            border: Border.all()),
-                        child: FlatButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                PageTransition(
-                                    child: Sector(),
-                                    type: PageTransitionType.fade));
-                          },
-                          //style: TextStyle(),
-                          child: Text(
-                            "Sector",
-                            style: TextStyle(
-                              fontFamily: 'ss',
-                              color: Colors.black,
-                              fontSize: size.width * 0.045,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: Colors.transparent,
-                            border: Border.all()),
-                        child: FlatButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                PageTransition(
-                                    child: Province(),
-                                    type: PageTransitionType.fade));
-                          },
-                          //style: TextStyle(),
-                          child: Text(
-                            "Province",
-                            style: TextStyle(
-                              fontFamily: 'ss',
-                              color: Colors.black,
-                              fontSize: size.width * 0.045,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ]),
-            ),
-          ),
-          SizedBox(height: size.height * 0.02),
-          Divider(
-            indent: size.width * 0.1,
-            endIndent: size.width * 0.1,
-            thickness: size.height * 0.001,
-            color: primaryColor,
-          ),
-          SizedBox(height: size.height * 0.01),
-          Text(
-            "Some of The Best Schools are:",
-            style: TextStyle(
-              fontFamily: 'ss',
-              fontSize: size.width * 0.05,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-            ),
+                letterSpacing: 1.0),
           ),
           Expanded(
               child: StreamBuilder<QuerySnapshot>(
-            stream: (searchString == null || searchString.trim() == "")
+            stream: (widget.province == null || widget.province.trim() == "")
                 ? Firestore.instance.collection('requests').snapshots()
                 : Firestore.instance
                     .collection('requests')
+                    .where("province", isEqualTo: widget.province)
                     .where("searchIndex", arrayContains: searchString)
                     .snapshots(),
             builder: (context, snapshot) {
@@ -330,23 +168,6 @@ class _SearchScreenState extends State<SearchScreen> {
             },
           )),
         ],
-      ),
-    );
-  }
-
-  Widget builResultCard(e) {
-    return Card(
-      color: Colors.black,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      elevation: 2.0,
-      child: Container(
-        child: Center(
-          child: Text(
-            e['name'],
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black, fontSize: 20.0),
-          ),
-        ),
       ),
     );
   }
